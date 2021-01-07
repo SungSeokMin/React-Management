89cd844f26e00532ee3231cd8f860e66b1216800
SungSeokMin <jkl154527@gmail.com>
2021-01-07 11:35:25 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './components/Customer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} alt="logo" />
-      <h2> gogo management system</h2>
-    </div>
-  );
+const customers = [
+  {
+    id: 1,
+    image: 'https://placeimg.com/64/64/1',
+    name: '홍길동',
+    birthday: 951127,
+    gender: '남',
+    job: '디자이너',
+  },
+  {
+    id: 2,
+    image: 'https://placeimg.com/64/64/2',
+    name: '성석민',
+    birthday: 951128,
+    gender: '남',
+    job: 'developer',
+  },
+  {
+    id: 3,
+    image: 'https://placeimg.com/64/64/3',
+    name: '이주용',
+    birthday: 951129,
+    gender: '남',
+    job: 'coffe',
+  },
+];
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {customers.map((customer) => {
+          return (
+            <Customer
+              key={customer.id}
+              id={customer.id}
+              image={customer.image}
+              name={customer.name}
+              birthday={customer.birthday}
+              gender={customer.gender}
+              job={customer.job}
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
